08f9ef5d867ab393e0c2124130db56d01253c786
kartiksinghbhati <kartiksingh0122@gmail.com>
2023-06-29 04:03:47 +0530

    Commit-1 UI

---
+++ src/components/Footer/Footer.jsx
@@ -0,0 +1,20 @@
+import React from 'react'
+import styles from './Footer.module.css';
+import Image from 'next/image';
+
+
+const Footer = () => {
+  return (
+    <div className={styles.container}>
+      <div>©2023 Kartik. All rights reserved.</div>
+      <div className={styles.social}>
+        <Image src="/1.png" width={15} height={15} className={styles.icon} alt="FacebookIcon" />
+        <Image src="/2.png" width={15} height={15} className={styles.icon} alt="InstagramIcon" />
+        <Image src="/3.png" width={15} height={15} className={styles.icon} alt="TwitterIcon" />
+        <Image src="/4.png" width={15} height={15} className={styles.icon} alt="YoutubeIcon" />
+      </div>
+    </div>
+  )
+}
+
+export default Footer
